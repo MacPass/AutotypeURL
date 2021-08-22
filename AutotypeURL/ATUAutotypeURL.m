cde557febdc72a614965cfa664c931b203685820
Rainer Burgstaller <rburgst@gmail.com>
2021-08-22 16:45:14 +0200

    Added support for Firefox
    fixes #11

--- AutotypeURL/ATUAutotypeURL.m
+++ AutotypeURL/ATUAutotypeURL.m
@@ -8,6 +8,7 @@
 
 #import "ATUAutotypeURL.h"
 #import "ATUSafariExtractor.h"
+#import "ATUFirefoxExtractor.h"
 #import "ATUChromeExtractor.h"
 #import "ATUSettingsViewController.h"
 
@@ -33,7 +34,7 @@ NSString *const kMPASettingsKeyFullMatch = @"kMPASettingsKeyFullMatch";
 - (instancetype)initWithPluginHost:(MPPluginHost *)host {
   self = [super initWithPluginHost:host];
   if(self) {
-    self.extractors = @[[[ATUChromeExtractor alloc] init], [[ATUSafariExtractor alloc] init]];
+    self.extractors = @[[[ATUChromeExtractor alloc] init], [[ATUSafariExtractor alloc] init], [[ATUFirefoxExtractor alloc] init]];
   }
   return self;
 }
